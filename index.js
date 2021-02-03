90e431cbd3d51d96e116df52e0f173da63b8f785
Shreya Amatya <sreya.amatya@gmail.com>
2021-02-03 14:36:25 +0545

    style added

--- index.js
+++ index.js
@@ -1,22 +1,9 @@
-const canvas = document.getElementById('canvas');
-const ctx = canvas.getContext('2d');
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
 
-const sizeInput = document.getElementById('size');
-const changeGridSize = document.getElementById('change-size');
-const scoreLabel = document.getElementById('score');
-
-const startScreen = document.getElementById('startScreen');
-const gameArea = document.querySelector(".gameArea");
-const sizebloc = document.querySelector(".size-bloc");
-
-// console.log(gameArea);
-// console.log(startScreen);
-
-//  startScreen.addEventListener('click', startGame);
-
- startScreen.onclick = function () {
-  startGame();
-}
+var sizeInput = document.getElementById('size');
+var changeSize = document.getElementById('change-size');
+var scoreLabel = document.getElementById('score');
 
 var score = 0;
 var size = 4;
@@ -26,13 +13,13 @@ var cells = [];
 var fontSize;
 var loss = false;
 
-var base =2;
+startGame();
 
-changeGridSize.onclick = function () {
-  base=document.getElementById('base').value*1;
+changeSize.onclick = function () {
   if (sizeInput.value >= 2 && sizeInput.value <= 20) {
     size = sizeInput.value;
     width = canvas.width / size - 6;
+    console.log(sizeInput.value);
     canvasClean();
     startGame();
   }
@@ -40,8 +27,8 @@ changeGridSize.onclick = function () {
 
 function cell(row, coll) {
   this.value = 0;
-  this.x = coll*width + 5 * (coll);
-  this.y = row* width + 5 * (row );
+  this.x = coll*width + 5 * (coll + 1);
+  this.y = row* width + 5 * (row + 1);
 }
 
 function createCells() {
@@ -58,12 +45,12 @@ function drawCell(cell) {
   ctx.beginPath();
   ctx.rect(cell.x, cell.y, width, width);
   switch (cell.value){
-//    case 0 :ctx.fillStyle = "#d6a3a3"; break;
+   case 0 :ctx.fillStyle = "#d6a3a3"; break;
         case 2 :ctx.fillStyle = "#FF5733"; break;
         case 4 :ctx.fillStyle = "#BDC81E"; break;
         case 8 :ctx.fillStyle = "#B133FF"; break;
         case 16 :ctx.fillStyle = "#1EC88C"; break;
-        case 32 :ctx.fillStyle = "#F7950C"; break;
+        case 32 :ctx.fillStyle = "#ADB725"; break;
         case 64 :ctx.fillStyle = "#D64475"; break;
         case 128 :ctx.fillStyle = "#D644B4"; break;
         case 256 :ctx.fillStyle = "#00ff8d"; break;
@@ -71,20 +58,6 @@ function drawCell(cell) {
         case 1024 :ctx.fillStyle = "#CCD644"; break;
         case 2048 :ctx.fillStyle = "#8DD644"; break;
         case 4096 :ctx.fillStyle = "#25B772"; break;
-        
-        
-        case 5 :ctx.fillStyle = "#FF5733"; break;
-        case 10:ctx.fillStyle = "#BDC81E"; break;
-        case 20 :ctx.fillStyle = "#B133FF"; break;
-        case 40 :ctx.fillStyle = "#1EC88C"; break;
-        case 80 :ctx.fillStyle = "#F7950C"; break;
-        case 160 :ctx.fillStyle = "#D64475"; break;
-        case 320 :ctx.fillStyle = "#D644B4"; break;
-        case 640 :ctx.fillStyle = "#00ff8d"; break;
-        case 1280 :ctx.fillStyle = "#D65E44"; break;
-        case 2560 :ctx.fillStyle = "#CCD644"; break;
-        case 5120 :ctx.fillStyle = "#8DD644"; break;
-        case 10240 :ctx.fillStyle = "#25B772"; break;
         default : ctx.fillStyle = "#ffffff";
   }
   ctx.fill();
@@ -117,12 +90,6 @@ document.onkeydown = function (event) {
 }
 
 function startGame() {
-  
-  // gameArea.classList.remove('hide');
-  sizebloc.classList.remove('hide');
-  startScreen.classList.add('hide');
-
-
   createCells();
   drawAllCells();
   pasteNewCell();
@@ -132,8 +99,6 @@ function startGame() {
 function finishGame() {
   canvas.style.opacity = '0.5';
   loss = true;
-  startScreen.classList.remove('hide');
-  startScreen.innerHTML= "GAME OVER !! <br> Your Final Score is <br>" + score ;
 }
 
 function drawAllCells() {
@@ -163,13 +128,7 @@ function pasteNewCell() {
     var row = Math.floor(Math.random() * size);
     var coll = Math.floor(Math.random() * size);
     if(!cells[row][coll].value) {
-      if(base==2)
-      {
-        cells[row][coll].value =2* Math.ceil(Math.random() * 2);
-      }
-      else{
-      cells[row][coll].value = base;
-      }
+      cells[row][coll].value = 2 * Math.ceil(Math.random() * 2);
       drawAllCells();
       return;
     }
