da888f9f898ae035f06d71aae97aa4dd9023b75e
Shreya Amatya <sreya.amatya@gmail.com>
2021-02-04 13:09:44 +0545

    cells drawn for base 5

--- index.js
+++ index.js
@@ -19,7 +19,6 @@ var loss = false;
 
 var base =2;
 
-startGame();
 
 changeGridSize.onclick = function () {
   base=document.getElementById('base').value*1;
@@ -64,6 +63,20 @@ function drawCell(cell) {
         case 1024 :ctx.fillStyle = "#CCD644"; break;
         case 2048 :ctx.fillStyle = "#8DD644"; break;
         case 4096 :ctx.fillStyle = "#25B772"; break;
+
+
+        case 5 :ctx.fillStyle = "#FF5733"; break;
+        case 10:ctx.fillStyle = "#BDC81E"; break;
+        case 20 :ctx.fillStyle = "#B133FF"; break;
+        case 40 :ctx.fillStyle = "#1EC88C"; break;
+        case 80 :ctx.fillStyle = "#F7950C"; break;
+        case 160 :ctx.fillStyle = "#D64475"; break;
+        case 320 :ctx.fillStyle = "#D644B4"; break;
+        case 640 :ctx.fillStyle = "#00ff8d"; break;
+        case 1280 :ctx.fillStyle = "#D65E44"; break;
+        case 2560 :ctx.fillStyle = "#CCD644"; break;
+        case 5120 :ctx.fillStyle = "#8DD644"; break;
+        case 10240 :ctx.fillStyle = "#25B772"; break;
         default : ctx.fillStyle = "#ffffff";
   }
   ctx.fill();
